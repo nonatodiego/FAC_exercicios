564ff9dd986d1990ca10e95c85ac25bbe5253562
Diego Nonato <42761103+nonatodiego@users.noreply.github.com>
2019-04-04 09:43:58 -0300

    Add files via upload

---
+++ lista3-exe1.cpp
@@ -0,0 +1,19 @@
+/* questão 01 - lista 3*/
+
+#include <stdio.h>
+
+int main () {
+	
+	//Declaração de variaveis
+	int n,y,i,m;
+	
+	//Leitura de dados
+	printf("Digite n e y: ");
+	scanf("%d %d",&n,&y);
+	
+	//Executar comandos
+	for(i=0;m<n;i++){
+		printf("%d",m);
+		m = y * i;		
+	}	
+}
